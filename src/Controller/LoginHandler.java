ebd35ee832fcf4c2f87d8a7e5dc5d87691c30645
koraybolat <kry342@gmail.com>
2017-12-12 22:19:12 +0300

    LoginHandler is created, main is created, LoginFrame was main fixed this issue
    Signed-off-by: koraybolat <kry342@gmail.com>

---
+++ src/Controller/LoginHandler.java
@@ -0,0 +1,40 @@
+package Controller;
+
+import View.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginHandler implements ActionListener{
+
+    /*LoginHandler have a unique id and password which is not connected to database*/
+    String id="admin";
+    String password="admin";
+    LoginFrame frame;
+
+
+    /*Just for indicate to take 2 string as arguments*/
+    public LoginHandler(LoginFrame frame){
+        this.frame = frame;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(frame.getUsernameField().getText()+"xd"+frame.getPasswordField().getText());
+        if(id.equals(frame.getUsernameField().getText()) && password.equals(frame.getPasswordField().getText())){
+            JOptionPane.showMessageDialog(frame, "You are successful");
+            createMainFrame();
+        }
+        else{
+            JOptionPane.showMessageDialog(frame,"Invalid Id & Password");
+        }
+    }
+
+    /*User successfully entered the Main.main frame, login frame is closed
+					* and new mainFrame is set to visible*/
+    public void createMainFrame(){
+        frame.setVisible(false);
+        MainFrame mainframe = new MainFrame();
+        mainframe.setVisible(true);
+    }
+}
